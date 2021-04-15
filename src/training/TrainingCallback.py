b6028c4935ccb4ff63f2f96cfbdcf9f8423782aa
MefAldemisov <alisha-omg@yandex.ru>
2021-04-15 10:36:34 +0300

    Training Callback is modified for the multiple implementations of Triplet loss algorithm

--- src/training/TrainingCallback.py
+++ src/training/TrainingCallback.py
@@ -8,8 +8,19 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 class TestCallback(callbacks.Callback):
 
-    def __init__(self, X, y, empty_model, threshold=0.2, input_size=500):
+    def __init__(self, X, y, empty_model=None, threshold=0.2, input_size=500, is_default=False):
+        """
+        Parameters:
+        - `X, y` - np.arrays with data (tokens) and labels (numerical representation of authors)
+        - `empty_model` - tensorflow.keras.model object, which will be used to copy the weights if the 'default'
+        type of triplet loss is used
+        -  `threshold` - alpha parameter of the triplet loss, threshold for the classification's distance
+        -  `input_size` - amount of tokens in one file
+        -  `is_default` - bool, if true, then it is necessary to generate the copy of the `self.model.weights`
+        to the `empty_model` model (due to the triplet nature of the model)
+        """
         super().__init__()
+        self.is_default = is_default
         self.threshold = threshold
         self.input_size = input_size
         index = np.where(y < 5)[0]
@@ -30,6 +41,7 @@ class TestCallback(callbacks.Callback):
     def apply_pca(self, transformed_x, n_authors=5):
         pca = PCA(n_components=3)
         x_pca = pca.fit_transform(transformed_x)
+        plt.figure(figsize=(10, 8))
         for developer in range(n_authors):
             indexes = np.where(self.y == developer)
             plt.plot(x_pca[indexes][0], x_pca[indexes][1], "o-", ms=3)
@@ -37,9 +49,11 @@ class TestCallback(callbacks.Callback):
         self.n += 1
 
     def on_epoch_end(self, epoch, logs=None):
-        self._recreate_model()
-
-        transformed_x = self.local_model.predict(self.X.reshape(-1, self.input_size))
+        if self.is_default:
+            self._recreate_model()
+            transformed_x = self.local_model.predict(self.X.reshape(-1, self.input_size))
+        else:
+            transformed_x = self.model.predict(self.X.reshape(-1, self.input_size))
 
         y_pred = []
         y_true = []
@@ -70,5 +84,3 @@ class TestCallback(callbacks.Callback):
 
     def return_results(self):
         return {"scores": self.scores, "recalls": self.recalls}
-
-
