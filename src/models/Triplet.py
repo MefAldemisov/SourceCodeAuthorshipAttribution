1375cbb553a5e44a53c02b1aec664e08af204cca
MefAldemisov <alisha-omg@yandex.ru>
2021-04-16 11:12:59 +0300

    Cosine distance added

--- src/models/Triplet.py
+++ src/models/Triplet.py
@@ -52,12 +52,36 @@ class Triplet(Model):
         y_pred = tf.convert_to_tensor(y_pred)
 
         anchor = y_pred[:, :self.output_size]
-        positive = y_pred[:, self.output_size:2*self.output_size]
-        negative = y_pred[:, 2*self.output_size:]
+        positive = y_pred[:, self.output_size:2 * self.output_size]
+        negative = y_pred[:, 2 * self.output_size:]
 
         positive_dist = tf.reduce_mean(tf.square(anchor - positive), axis=1)
         negative_dist = tf.reduce_mean(tf.square(anchor - negative), axis=1)
         return tf.maximum(positive_dist - negative_dist + alpha, 0.)
+
+    def get_distance(self, start, end, type="eucl"):
+        """
+        :param start:
+        :param end:
+        :param type: str in ["eucl", "cos"|]
+        :return:
+        """
+        assert type in ["eucl", "cos"]
+        if type == "eucl":
+            return tf.square(start - end)
+        elif type == "cos":
+            # angular distance(a, p) = a*p (element-wise)/sqrt(sum(square(a)))/sqrt(sum(square(p)))
+            # definition of cos distance https://reference.wolfram.com/language/ref/CosineDistance.html?view=all
+
+            length_func = lambda x: tf.sqrt(tf.reduce_sum(tf.square(x, axis=1)))
+            div_func = lambda x, y: tf.math.divide_no_nan(x, y)
+
+            product = tf.math.multiply(start, end, axis=1)
+            l_start, l_end = length_func(start), length_func(end)
+            distance = div_func(div_func(product, l_start), l_end)
+            return distance
+
+        return None
 
     def hard_triplet_loss(self, _, y_pred):
         """
@@ -68,7 +92,8 @@ class Triplet(Model):
         # select the array of dist(anchor, negative)
         alpha = 0.5
         selection_percent = 0.01
-        selection_num = selection_percent*self.output_size
+        dist = "cos"
+        selection_num = selection_percent * self.output_size
 
         y_pred = tf.convert_to_tensor(y_pred)
 
@@ -76,13 +101,14 @@ class Triplet(Model):
         positive = y_pred[:, self.output_size:2 * self.output_size]
         negative = y_pred[:, 2 * self.output_size:]
 
-        positive_dist = tf.square(anchor - positive)
-        negative_dist = tf.square(anchor - negative)
+        positive_dist = self.get_distance(anchor, positive, dist)
+        negative_dist = self.get_distance(anchor, negative, dist)
 
         # sort both arrays
-        sorted_pos = tf.sort(positive_dist, axis=1, direction='DESCENDING')[:selection_num]
-        sorted_neg = tf.sort(negative_dist, axis=1, direction='ASCENDING')[:selection_num]
-        return tf.maximum(tf.reduce_mean(sorted_pos, axis=1) - tf.reduce_mean(sorted_neg, axis=1)+alpha, 0.)
+        # sorted_pos = tf.sort(positive_dist, axis=1, direction='DESCENDING')[:selection_num]
+        # sorted_neg = tf.sort(negative_dist, axis=1, direction='ASCENDING')[:selection_num]
+
+        return tf.maximum(positive_dist - negative_dist + alpha, 0.)
 
     def create_triplet_model(self):
         input_anchor = layers.Input(shape=self.input_size)
@@ -117,10 +143,10 @@ class Triplet(Model):
     def train(self, batch_size: int = 128, epochs: int = 100,
               lr_patience: int = 3, stopping_patience: int = 12):
 
-        X_train, x_test,  y_train, y_test = self.preprocess()
+        X_train, x_test, y_train, y_test = self.preprocess()
         is_default = self.triplet_type == "default"
         triplet = self.create_triplet_model() if is_default else self.model
-        steps_per_epoch = int(X_train.shape[0]/batch_size)
+        steps_per_epoch = int(X_train.shape[0] / batch_size)
         optimizer = optimizers.Adam(0.1)
         lr_schedule = callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5,
                                                   patience=lr_patience,
